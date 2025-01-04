263482d9334c6d8ccdc300cf9a9c7b14940470d8
Adam Hernandez <adam@digimedia.com>
2025-01-03 22:39:50 -0600

    .

--- gems/notices_bot/lib/notices_bot.rb
+++ gems/notices_bot/lib/notices_bot.rb
@@ -3,21 +3,21 @@ class NoticesBot
   end
 
   def monitor
-    # Thread.new {
-    #   while true
-    #     variable = Variable.where(key: :notices_last_sent_at).first_or_initialize
-    #     variable.value = 2.days.ago if variable.value.blank?
-    #     if Time.now.hour >= 18 && Time.parse(variable.value) < 1.day.ago
-    #       sleep 1
-    #       next
-    #     end
-    #     variable.value = Time.now
-    #     next if !variable.save
-    #     Notice.order(:order).each do |notice|
-    #       $tx_bot.send_text(notice.message, notice.ch_index)
-    #     end
-    #   end
-    # }
+    Thread.new {
+      while true
+        variable = Variable.where(key: :notices_last_sent_at).first_or_initialize
+        variable.value = 2.days.ago if variable.value.blank?
+        if Time.now.hour >= 18 && Time.parse(variable.value) > 1.day.ago
+          sleep 1
+          next
+        end
+        variable.value = Time.now
+        next if !variable.save
+        Notice.order(:order).each do |notice|
+          $tx_bot.send_text(notice.message, notice.ch_index)
+        end
+      end
+    }
     self
   end
 end
