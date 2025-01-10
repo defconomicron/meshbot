767145236039b27d5c25463488dedb077795f72b
Adam Hernandez <adam@digimedia.com>
2025-01-09 22:07:13 -0600

    .

--- gems/meshtastic_cli/lib/meshtastic_cli.rb
+++ gems/meshtastic_cli/lib/meshtastic_cli.rb
@@ -11,6 +11,10 @@ class MeshtasticCli
     `#{$meshtastic_path} --host #{@host} --reboot`
   end
 
+  def get_value(str, key)
+    str.scan(/['"]?#{key}['"]?: ['"]?(.*?)['"]?([\,\s\n\}]|$)/).flatten.first rescue nil
+  end
+
   def responses(&block)
     $rx_bot.log 'IGNORING RESPONSES FOR 30 SECONDS...', :yellow
     deaf = true
@@ -20,27 +24,52 @@ class MeshtasticCli
       $rx_bot.log 'NO LONGER IGNORING RESPONSES!', :yellow
     }
     PTY.spawn("#{$meshtastic_path} --host #{@host} --listen") do |stdout, stdin, pid|
-      response = nil
+      response = ''
       stdout.each do |line|
         next if deaf
         # $rx_bot.log "RAW: #{line.strip}"
-        str = line.strip.force_encoding('UTF-8')
-        raise Exception.new(str) if error?(str)
-        if str =~ /DEBUG/
-          response = str << "\n"
-        elsif response.present?
-          response << str << "\n"
-          if str.blank?
-            response = case response
-              when /packet/ then response.split('packet')[1].strip rescue ''
-              when /node_info/ then response.split('node_info')[1].strip rescue ''
-              else response
-            end
-            response = JSON.parse(JSON.repair(response)) rescue nil
-            yield response if response.present?
-            response = nil
-          end
+        line = line.strip.force_encoding('UTF-8')
+        raise Exception.new(line) if error?(line)
+        if line =~ /DEBUG/ && response.present?
+          _response = {
+            id:                  get_value(response, 'id'),
+            from:                get_value(response, 'from'),
+            to:                  get_value(response, 'to'),
+            short_name:          get_value(response, 'short_name').presence || get_value(response, 'shortName'),
+            long_name:           get_value(response, 'long_name').presence || get_value(response, 'longName'),
+            portnum:             get_value(response, 'portnum'),
+            macaddr:             get_value(response, 'macaddr'),
+            hw_model:            get_value(response, 'hw_model').presence || get_value(response, 'hwModel'),
+            rx_time:             get_value(response, 'rx_time').presence || get_value(response, 'rxTime'),
+            priority:            get_value(response, 'priority'),
+            via_mqtt:            get_value(response, 'via_mqtt').presence || get_value(response, 'viaMqtt'),
+            hop_start:           get_value(response, 'hop_start').presence || get_value(response, 'hopStart'),
+            latitude:            get_value(response, 'latitude'),
+            longitude:           get_value(response, 'longitude'),
+            rx_snr:              get_value(response, 'rx_snr').presence || get_value(response, 'rxSnr'),
+            rx_rssi:             get_value(response, 'rx_rssi').presence || get_value(response, 'rxRssi'),
+            hop_limit:           get_value(response, 'hop_limit').presence || get_value(response, 'hopLimit'),
+            altitude:            get_value(response, 'altitude'),
+            time:                get_value(response, 'time'),
+            channel:             get_value(response, 'channel'),
+            location_source:     get_value(response, 'location_source').presence || get_value(response, 'locationSource'),
+            ground_speed:        get_value(response, 'ground_speed').presence || get_value(response, 'groundSpeed'),
+            ground_track:        get_value(response, 'ground_track').presence || get_value(response, 'groundTrack'),
+            precision_bits:      get_value(response, 'precision_bits').presence || get_value(response, 'precisionBits'),
+            latitude_i:          get_value(response, 'latitude_i').presence || get_value(response, 'latitudeI'),
+            longitude_i:         get_value(response, 'longitude_i').presence || get_value(response, 'longitudeI'),
+            bitfield:            get_value(response, 'bitfield'),
+            battery_level:       get_value(response, 'battery_level').presence || get_value(response, 'batteryLevel'),
+            voltage:             get_value(response, 'voltage'),
+            channel_utilization: get_value(response, 'channel_utilization').presence || get_value(response, 'channelUtilization'),
+            air_util_tx:         get_value(response, 'air_util_tx').presence || get_value(response, 'airUtilTx'),
+            uptime_seconds:      get_value(response, 'uptime_seconds').presence || get_value(response, 'uptimeSeconds'),
+            payload:             get_value(response, 'payload')
+          }.select {|k,v| v.present?}.with_indifferent_access
+          yield _response if response.present? && response =~ /packet {/
+          response = ''
         end
+        response << line << "\n"
       end
     end
   end
--- gems/rx_bot/lib/rx_bot.rb
+++ gems/rx_bot/lib/rx_bot.rb
@@ -12,51 +12,43 @@ class RxBot
     Thread.new {
       begin
         MeshtasticCli.new(host: @host, name: @name).responses do |response|
-          packet = response
-          next if !packet.is_a?(Hash)
-          node = Node.where(number: packet['num'].presence || packet['from']).first_or_initialize
+          next if !response.is_a?(Hash)
+          next if response['num'].blank? && response['from'].blank?
+          node = Node.where(number: response['num'].presence || response['from']).first_or_initialize
           next if node.ignore? || node.short_name == $tx_bot.name
-          if packet.keys.include?('user')
-            nodeinfo_snapshot = packet.to_json
-            log "RX: #{nodeinfo_snapshot}", :blue
-            node.nodeinfo_snapshot = nodeinfo_snapshot
-            user_snapshot = packet['user'].to_json
-            log "RX: #{user_snapshot}", :blue
-            node.user_snapshot = user_snapshot
-          end
-          if packet.keys.include?('position')
-            position_snapshot = packet['position'].to_json
-            log "RX: #{position_snapshot}", :blue
-            node.position_snapshot = position_snapshot
-          end
-          if packet.keys.include?('device_metrics')
-            device_metrics_snapshot = packet['device_metrics'].to_json
-            log "RX: #{device_metrics_snapshot}", :blue
-            node.device_metrics_snapshot = device_metrics_snapshot
-          end
           node.updated_at = Time.now
           node.save
-          if packet.keys.include?('decoded')
-            ch_index = channel = packet['channel'] rescue nil
-            payload = packet['decoded']['payload'] rescue nil
-            ch_index ||= 0
-            log "RX: #{packet}", :blue
-            payload = "#{payload}".strip
-            Message.create(ch_index: ch_index, node_id: node.id, message: payload)
-            params_arr = [payload.split(' ')[1..-1]].compact.flatten
-            params_str = params_arr.join(' ')
-            $TEXT_MESSAGE_HANDLERS.each {|handler|
-              [handler.call(payload: payload, params_arr: params_arr, params_str: params_str, ch_index: ch_index, node: node)].flatten.compact.each do |text|
-                $tx_bot.send_text(text, ch_index) if text.present?
-              end
-            }
+          log "RX: #{response}", :blue
+          case response['portnum']
+            when 'TEXT_MESSAGE_APP'
+              ch_index = channel = response['channel'] rescue nil
+              payload = response['payload'] rescue nil
+              ch_index ||= 0
+              payload = "#{payload}".strip
+              Message.create(ch_index: ch_index, node_id: node.id, message: payload)
+              params_arr = [payload.split(' ')[1..-1]].compact.flatten
+              params_str = params_arr.join(' ')
+              $TEXT_MESSAGE_HANDLERS.each {|handler|
+                [handler.call(payload: payload, params_arr: params_arr, params_str: params_str, ch_index: ch_index, node: node)].flatten.compact.each do |text|
+                  $tx_bot.send_text(text, ch_index) if text.present?
+                end
+              }
+            when 'POSITION_APP'
+              node.position_snapshot = response.to_json
+              node.save
+            when 'TELEMETRY_APP'
+              node.telemetry_snapshot = response.to_json
+              node.save
+            when 'NODEINFO_APP'
+              node.nodeinfo_snapshot = response.to_json
+              node.save
           end
         end
       rescue Exception => e
         log "EXCEPTION: #{e}: #{e.backtrace}", :red
         log "Whew! I'm going to sleep... Be back in a minute.", :yellow
         sleep 1
-        log "Okay, I'm awake again and listening for new packets!", :yellow
+        log "Okay, I'm awake again and listening for new responses!", :yellow
         retry
       end
     }
