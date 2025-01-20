64bcd52685ada50dffc0b7f7f200a03d1692042e
Adam Hernandez <adam@digimedia.com>
2025-01-19 21:49:11 -0600

    .

--- gems/recall/lib/recall.rb
+++ gems/recall/lib/recall.rb
@@ -8,6 +8,7 @@ class Recall
 
   def msg
     return 'A keyword must be provided with your request.  Example: @recall hello world' if @keyword.blank?
+    return 'Sorry, before you can use this command, I must first know who you are.  Try again later.' if @requester_node.nil?
     rx_bot_node = Node.where(short_name: $rx_bot.name).last
     message = Message.where('message like ? and node_id != ? and node_id != ?', "%#{@keyword}%", @requester_node.id, rx_bot_node.id).last
     message.nil? ? "No messages found matching: %#{@keyword}%" : "#{message.node.long_name.presence || "Node ##{message.node.number}"}: #{message.message}"
