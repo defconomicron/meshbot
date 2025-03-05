db57778151d4f16c2bd7290b74e7009932aacba0
Adam Hernandez <adam@digimedia.com>
2025-03-04 21:56:08 -0600

    .

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require './config/environment.rb'
 $log_it = LogIt.new
 $log_it.log('ERROR: settings.yml not defined', :red) if $settings.blank?
 $log_it.log('ERROR: meshtastic path not defined in settings.yml', :red) if $meshtastic_path.blank?
-# Process.daemon(true, false)
+Process.daemon(true, false)
 $log_it.log('ERROR: bot not defined in settings.yml', :red) if $settings['bot'].blank?
 $rx_bot = RxBot.new(name: $settings['bot']['rx']['name'], host: $settings['bot']['rx']['host'])
 $tx_bot = TxBot.new(name: $settings['bot']['tx']['name'], host: $settings['bot']['tx']['host'])
