e574d196711a1abe94ed9510b84fda46e50b2a68
Adam Hernandez <adam@digimedia.com>
2025-03-12 22:41:36 -0500

    .

--- gems/help/lib/help.rb
+++ gems/help/lib/help.rb
@@ -6,11 +6,9 @@ class Help
 
   def msg
     commands = ['Greetings human!  I am a service droid.  Hear me r0ar!  My commands are: ']
-    cmds = []
-    $COMMAND_KEYWORDS.each_slice(15) do |batch|
-      cmds << batch.join(', ')
-    end
+    cmds = $COMMAND_KEYWORDS.each_slice(15).map {|batch| batch.join(', ')}
     commands[0] << cmds[0]
     commands += cmds[1..-1]
+    commands
   end
 end
