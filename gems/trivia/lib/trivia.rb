b7182f987f4cbf2b3d4a54d5e4c80728919c2fac
Adam Hernandez <adam@digimedia.com>
2025-03-18 13:20:48 -0500

    .

--- gems/trivia/lib/trivia.rb
+++ gems/trivia/lib/trivia.rb
@@ -1,150 +1,114 @@
+require 'yaml'
+$settings = YAML.load_file('settings.yml') rescue {}
+raise Exception.new('settings.yml not defined') if $settings.blank?
+
 ($COMMAND_KEYWORDS ||= []) << '@trivia'
 $TRIVIA = nil
-$QUESTION_NUMBER = 0
-$DEFAULT_MAX_QUESTIONS = 25
-$MAX_QUESTIONS = 25
+$TRIVIA_INCORRECT_CH_INDEX_MSG = $settings['trivia']['incorrect_ch_index_msg'].presence || 'Trivia cannot be played on this channel.'
 
 ($TEXT_MESSAGE_HANDLERS ||= []) << Proc.new {|args|
-  trivia_answer = $TRIVIA_ANSWER
-
-  if /^@trivia/i =~ args[:payload]
-    if args[:ch_index].to_i != 2
-      $tx_bot.send_text('To play trivia, you must first join the channel named "Trivia" with a PSK of "AQ=="', args[:ch_index])
-      next nil
-    end
+  node = args[:node]
+  ch_index = args[:ch_index]
+  params_str = args[:params_str]
+  payload = args[:payload]
+  if /^@trivia/i =~ payload
     if $TRIVIA.present?
-      $tx_bot.send_text('Trivia is already running!', args[:ch_index])
+      $tx_bot.send_text('Trivia is already running!', @ch_index)
       next nil
     end
-    $TRIVIA = Trivia.new(ch_index: args[:ch_index])
-    $QUESTION_NUMBER = 0
-    $MAX_QUESTIONS = args[:params_str].present? && args[:params_str].to_i > 0 ? args[:params_str].to_i : $DEFAULT_MAX_QUESTIONS
-    TriviaProfile.delete_all
-    $tx_bot.send_text("New trivia game is about to start!", args[:ch_index])
-    $tx_bot.send_text("The player with the most number of points after #{$MAX_QUESTIONS} questions wins!", args[:ch_index])
-    $TRIVIA.new_question
+    $TRIVIA = Trivia.new(ch_index: ch_index, max_questions: params_str).start
   end
-
-  if /^@score|@points$/i =~ args[:payload]
-    if $TRIVIA.nil?
-      $tx_bot.send_text("Trivia has not yet started.", args[:ch_index])
-      next nil
-    end
-    points = args[:node].trivia_profile.try(:points) || 0
-    $tx_bot.send_text("Your score in trivia is currently: #{points}", args[:ch_index])
+  if $TRIVIA.nil? && (/^@score|@points$/i =~ payload || /^@hint|@clue$/i =~ payload || /^@next|@skip$/i =~ payload)
+    $tx_bot.send_text('Trivia has not yet started.', @ch_index)
+    next nil
   end
-
-  if /^@hint|@clue$/i =~ args[:payload]
-    if $TRIVIA.nil?
-      $tx_bot.send_text("Trivia has not yet started.", args[:ch_index])
-      next nil
-    end
-    clue = 'x' * trivia_answer.length
-    clue[0] = trivia_answer[0]
-    clue[1] = trivia_answer[1]
-    clue[2] = trivia_answer[2]
-    cost = (trivia_answer.length / 2.0).round
-    $tx_bot.send_text("You have purchased a clue for #{cost} points.", args[:ch_index])
-    $tx_bot.send_text("Clue: #{clue}", args[:ch_index])
-    trivia_profile = TriviaProfile.where(node_id: args[:node].id).first_or_initialize
-    trivia_profile.points -= cost
-    trivia_profile.save
-  end
-
-  if /^@next|@skip$/i =~ args[:payload]
-    if $TRIVIA.nil?
-      $tx_bot.send_text("Trivia has not yet started.", args[:ch_index])
-      next nil
-    end
-    $TAUNT_THREAD.exit if !$TAUNT_THREAD.nil? && $TAUNT_THREAD.alive?
-    $TIMES_UP_THREAD.exit if !$TIMES_UP_THREAD.nil? && $TIMES_UP_THREAD.alive?
-    $TRIVIA_ANSWER = nil
-    $tx_bot.send_text("Question skipped!", args[:ch_index])
-    $TRIVIA.new_question
-  end
-
-  if trivia_answer.present? && trivia_answer.downcase.strip == args[:payload].downcase.strip
-    $TAUNT_THREAD.exit if !$TAUNT_THREAD.nil? && $TAUNT_THREAD.alive?
-    $TIMES_UP_THREAD.exit if !$TIMES_UP_THREAD.nil? && $TIMES_UP_THREAD.alive?
-    $TRIVIA_ANSWER = nil
-    name = [args[:node].short_name, args[:node].long_name].select(&:present?).join(': ').presence || "Node ##{args[:node].number}"
-    trivia_profile = TriviaProfile.where(node_id: args[:node].id).first_or_initialize
-    trivia_profile.points += trivia_answer.length
-    trivia_profile.save
-    $tx_bot.send_text("#{Trivia::WINNER_RESPONSES.sample} #{name} got the answer right!", args[:ch_index])
-    $tx_bot.send_text("You now have #{trivia_profile.points} point(s)", args[:ch_index])
-    $TRIVIA.new_question
-  end
-
+  next nil if $TRIVIA.nil?
+  $TRIVIA.score(node) if /^@score|@points$/i =~ payload
+  $TRIVIA.hint(node) if /^@hint|@clue$/i =~ payload
+  $TRIVIA.skip if /^@next|@skip$/i =~ payload
+  $TRIVIA.answer(node) if $TRIVIA.trivia_answer.downcase.strip == payload.downcase.strip
   nil
 }
 
 class Trivia
-  WINNER_RESPONSES = [
-    'Impressive!',
-    'Nice!',
-    'Wow we got a genius over here!',
-    'Well done!',
-    'DANGER!: SMART PERSON DETECTED!',
-    'Superb!',
-    'WOW!',
-    'Way to go!',
-    'Go, you!!',
-    'Boom goes the dynamite!',
-    "OOooohh!! Ya'll just got dunked on!",
-    'Swish!',
-    'Booomm!!!',
-    'You got it!',
-  ]
+  attr_accessor :trivia_answer
 
-  TAUNT_RESPONSES = [
-    'Hello?',
-    'Anyone? Anyone?',
-    'Bueller?',
-    'Anyone?',
-    'Come on, you know this one.',
-    'My grandma could answer this one!',
-    'My newborn could answer this one.',
-    'Zzz... Hello?',
-    'This is the easiest question I have!',
-    'Uuuuhhhh.... errmmm... hello?',
-    'Lol the answer is so easy!',
-    'This has to be the easiest question ever!',
-    'This is a commonly asked question in 1st grade.',
-    "Ya'll should know this one...",
-    'The answer is so simple.'
-  ]
-
-  LOSER_RESPONSES = [
-    'Bruh, the answer was',
-    'Bro, the answer was',
-    'Dude, the answer was',
-    'Brah, the answer was',
-    'Heh... The answer was',
-    "Time's up! The answer was",
-    'Hmmm... the answer was',
-    'Hello? The answer was',
-    'So sorry! The answer was',
-    'So like, the answer was',
-    'No..., no.. am sorry... the answer was',
-    'The answer was',
-  ]
+  WINNER_RESPONSES = File.readlines("#{File.dirname(__FILE__)}/winner_responses.dat")
+  TAUNT_RESPONSES = File.readlines("#{File.dirname(__FILE__)}/taunt_responses.dat")
+  LOSER_RESPONSES = File.readlines("#{File.dirname(__FILE__)}/loser_responses.dat")
 
   def initialize(options)
     @ch_index = options[:ch_index]
+    @max_questions = options[:max_questions].present? && options[:max_questions].to_i > 0 ? options[:max_questions].to_i : ($settings['trivia']['default_max_questions'].presence || 25)
+    @question_number = 0
+  end
+
+  def start
+    if @ch_index.to_i != $settings['trivia']['ch_index']
+      $tx_bot.send_text($TRIVIA_INCORRECT_CH_INDEX_MSG, @ch_index)
+      return
+    end
+    @question_number = 0
+    TriviaProfile.delete_all
+    $tx_bot.send_text('New trivia game is about to start!', @ch_index)
+    $tx_bot.send_text("The player with the most number of points after #{@max_questions} questions wins!", @ch_index)
+    new_question
+    self
+  end
+
+  def score(node)
+    points = node.trivia_profile.try(:points) || 0
+    $tx_bot.send_text("Your score in trivia is currently: #{points}", @ch_index)
+    self
+  end
+
+  def hint(node)
+    clue = 'x' * @trivia_answer.length
+    clue[0] = @trivia_answer[0]
+    clue[1] = @trivia_answer[1]
+    clue[2] = @trivia_answer[2]
+    cost = (@trivia_answer.length / 2.0).round
+    $tx_bot.send_text("You have purchased a clue for #{cost} points.", @ch_index)
+    $tx_bot.send_text("Clue: #{clue}", @ch_index)
+    trivia_profile = TriviaProfile.where(node_id: node.id).first_or_initialize
+    trivia_profile.points -= cost
+    trivia_profile.save
+    self
+  end
+
+  def skip
+    @trivia_answer = nil
+    $TAUNT_THREAD.exit if !$TAUNT_THREAD.nil? && $TAUNT_THREAD.alive?
+    $TIMES_UP_THREAD.exit if !$TIMES_UP_THREAD.nil? && $TIMES_UP_THREAD.alive?
+    $tx_bot.send_text('Question skipped!', @ch_index)
+    new_question
+    self
+  end
+
+  def answer(node)
+    trivia_answer = @trivia_answer
+    @trivia_answer = nil
+    $TAUNT_THREAD.exit if !$TAUNT_THREAD.nil? && $TAUNT_THREAD.alive?
+    $TIMES_UP_THREAD.exit if !$TIMES_UP_THREAD.nil? && $TIMES_UP_THREAD.alive?
+    trivia_profile = TriviaProfile.where(node_id: node.id).first_or_initialize
+    trivia_profile.points += trivia_answer.length
+    trivia_profile.save
+    $tx_bot.send_text("#{Trivia::WINNER_RESPONSES.sample} #{node.name} got the answer right!", @ch_index)
+    $tx_bot.send_text("You now have #{trivia_profile.points} point(s)", @ch_index)
+    new_question
+    self
   end
 
   def new_question
-    $QUESTION_NUMBER += 1
-
-    if $QUESTION_NUMBER <= $MAX_QUESTIONS
-      $tx_bot.send_text("Get ready for question ##{$QUESTION_NUMBER}!", @ch_index)
+    @question_number += 1
+    if @question_number <= @max_questions
+      $tx_bot.send_text("Get ready for question ##{@question_number}!", @ch_index)
     else
+      @trivia_answer = nil
       $TRIVIA = nil
-      $TRIVIA_ANSWER = nil
-      $tx_bot.send_text("GAME OVER!! Preparing the results...", @ch_index)
+      $tx_bot.send_text('GAME OVER!! Preparing the results...', @ch_index)
       trivia_profiles = TriviaProfile.order('points desc')
-      $tx_bot.send_text("Hmm... It looks like no one scored this round. Lol.  Better luck next time!", @ch_index) if trivia_profiles.empty?
+      $tx_bot.send_text('Hmm... It looks like no one scored this round. Lol.  Better luck next time!', @ch_index) if trivia_profiles.empty?
       trivia_profiles.each_with_index do |trivia_profile, i|
         node = trivia_profile.node
         name = node.name
@@ -154,30 +118,16 @@ class Trivia
           when 2 then $tx_bot.send_text("[3rd Place]: #{name} with #{trivia_profile.points} points", @ch_index)
         end
       end
-      $tx_bot.send_text("To play again, say @trivia", @ch_index)
+      $tx_bot.send_text('To play again, say @trivia', @ch_index)
       return
     end
-
-    $TRIVIA_QUESTION, $TRIVIA_ANSWER = File.readlines("#{File.dirname(__FILE__)}/trivia.dat").sample.split('*')
-
-    $TRIVIA_QUESTION.strip!
-    trivia_question = $TRIVIA_QUESTION
-
-    $TRIVIA_ANSWER.strip!
-    trivia_answer = $TRIVIA_ANSWER
-
-    $log_it.log "TRIVIA_QUESTION = #{trivia_question} | TRIVIA_ANSWER = #{trivia_answer}"
-    $tx_bot.send_text("For #{trivia_answer.length} points, #{trivia_question}", @ch_index)
-
-    $TAUNT_THREAD = Thread.new {
-      sleep 60
-      $tx_bot.send_text(Trivia::TAUNT_RESPONSES.sample, @ch_index)
-    }
-
-    $TIMES_UP_THREAD = Thread.new {
-      sleep 120
-      $tx_bot.send_text("#{Trivia::LOSER_RESPONSES.sample}: #{trivia_answer}", @ch_index)
-      new_question
-    }
+    @trivia_question, @trivia_answer = File.readlines("#{File.dirname(__FILE__)}/trivia.dat").sample.split('*')
+    @trivia_question.strip!
+    @trivia_answer.strip!
+    $log_it.log "TRIVIA_QUESTION = #{@trivia_question} | TRIVIA_ANSWER = #{@trivia_answer}"
+    $tx_bot.send_text("For #{@trivia_answer.length} points, #{@trivia_question}", @ch_index)
+    $TAUNT_THREAD = Thread.new {sleep 60;$tx_bot.send_text(Trivia::TAUNT_RESPONSES.sample, @ch_index)}
+    $TIMES_UP_THREAD = Thread.new {sleep 120;$tx_bot.send_text("#{Trivia::LOSER_RESPONSES.sample}: #{@trivia_answer}", @ch_index);new_question}
+    self
   end
 end
--- main.rb
+++ main.rb
@@ -8,35 +8,35 @@ begin
   $log_it.log 'settings.yml found', :green
 
   $meshtastic_path = $settings['meshtastic']['path'] rescue nil
-  raise Exception.new('meshtastic > path not defined in settings.yml') if $meshtastic_path.blank?
-  $log_it.log 'meshtastic > path found in settings.yml', :green
+  raise Exception.new('meshtastic => path not defined in settings.yml') if $meshtastic_path.blank?
+  $log_it.log 'meshtastic => path found in settings.yml', :green
 
   raise Exception.new('bot not defined in settings.yml') if $settings['bot'].blank?
   $log_it.log 'bot found in settings.yml', :green
 
-  raise Exception.new('bot > rx not defined in settings.yml') if ($settings['bot']['rx'] rescue nil).blank?
-  $log_it.log 'bot > rx found in settings.yml', :green
-  raise Exception.new('bot > rx > name not defined in settings.yml') if ($settings['bot']['rx']['name'] rescue nil).blank?
-  $log_it.log 'bot > rx > name found in settings.yml', :green
-  raise Exception.new('bot > rx > host not defined in settings.yml') if ($settings['bot']['rx']['host'] rescue nil).blank?
-  $log_it.log 'bot > rx > host found in settings.yml', :green
+  raise Exception.new('bot => rx not defined in settings.yml') if ($settings['bot']['rx'] rescue nil).blank?
+  $log_it.log 'bot => rx found in settings.yml', :green
+  raise Exception.new('bot => rx => name not defined in settings.yml') if ($settings['bot']['rx']['name'] rescue nil).blank?
+  $log_it.log 'bot => rx => name found in settings.yml', :green
+  raise Exception.new('bot => rx => host not defined in settings.yml') if ($settings['bot']['rx']['host'] rescue nil).blank?
+  $log_it.log 'bot => rx => host found in settings.yml', :green
   $rx_bot = RxBot.new(name: $settings['bot']['rx']['name'], host: $settings['bot']['rx']['host'])
 
-  raise Exception.new('bot > tx not defined in settings.yml') if ($settings['bot']['tx'] rescue nil).blank?
-  $log_it.log 'bot > tx found in settings.yml', :green
-  raise Exception.new('bot > tx > name not defined in settings.yml') if ($settings['bot']['tx']['name'] rescue nil).blank?
-  $log_it.log 'bot > tx > name found in settings.yml', :green
-  raise Exception.new('bot > tx > host not defined in settings.yml') if ($settings['bot']['tx']['host'] rescue nil).blank?
-  $log_it.log 'bot > tx > host found in settings.yml', :green
+  raise Exception.new('bot => tx not defined in settings.yml') if ($settings['bot']['tx'] rescue nil).blank?
+  $log_it.log 'bot => tx found in settings.yml', :green
+  raise Exception.new('bot => tx => name not defined in settings.yml') if ($settings['bot']['tx']['name'] rescue nil).blank?
+  $log_it.log 'bot => tx => name found in settings.yml', :green
+  raise Exception.new('bot => tx => host not defined in settings.yml') if ($settings['bot']['tx']['host'] rescue nil).blank?
+  $log_it.log 'bot => tx => host found in settings.yml', :green
   $tx_bot = TxBot.new(name: $settings['bot']['tx']['name'], host: $settings['bot']['tx']['host'])
 
   $max_text_length = $settings['bot']['tx']['max_text_length'] rescue nil
-  raise Exception.new('bot > tx > max_text_length not defined in settings.yml') if $max_text_length.blank?
-  $log_it.log 'bot > tx > max_text_length found in settings.yml', :green
+  raise Exception.new('bot => tx => max_text_length not defined in settings.yml') if $max_text_length.blank?
+  $log_it.log 'bot => tx => max_text_length found in settings.yml', :green
 
   $sending_tries = $settings['bot']['tx']['sending_tries'] rescue nil
-  raise Exception.new('bot > tx > sending_tries not defined in settings.yml') if $sending_tries.blank?
-  $log_it.log 'bot > tx > sending_tries found in settings.yml', :green
+  raise Exception.new('bot => tx => sending_tries not defined in settings.yml') if $sending_tries.blank?
+  $log_it.log 'bot => tx => sending_tries found in settings.yml', :green
 
   $log_it.log "[#{$rx_bot.name}] MONITORING!", :yellow
   $rx_bot.monitor
